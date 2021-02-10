a322d8f2cd2ad53649b45db3365d77b7102c417f
Explosionman <rybinskov@gmail.com>
2021-02-10 14:56:38 +0300

    hw2

---
+++ first-web-app/src/main/java/ru/rybinskov/error_servlet/Error404Servlet.java
@@ -0,0 +1,16 @@
+//package ru.rybinskov.error_servlet;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebFilter(urlPatterns = "/404")
+//public class Error404Servlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+//    }
+//}
