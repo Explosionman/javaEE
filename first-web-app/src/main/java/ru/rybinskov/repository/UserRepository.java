b80184f8a566c9db1f1370299e59d270aa305093
Explosionman <rybinskov@gmail.com>
2021-02-24 14:49:57 +0300

    переделал проект под JSF

--- first-web-app/src/main/java/ru/rybinskov/repository/UserRepository.java
+++ first-web-app/src/main/java/ru/rybinskov/repository/UserRepository.java
@@ -1,18 +1,30 @@
 package ru.rybinskov.repository;
 
+import ru.rybinskov.persist.Role;
 import ru.rybinskov.persist.User;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Named
+@ApplicationScoped
 public class UserRepository {
 
     private final Map<Long, User> userMap = new ConcurrentHashMap<>();
 
     private final AtomicLong identity = new AtomicLong(0);
+
+    @PostConstruct
+    public void init() {
+        this.saveOrUpdate(new User(null, "Alex", "123", Role.ROLE_ADMIN));
+        this.saveOrUpdate(new User(null, "user", "user", Role.ROLE_USER));
+    }
 
     public List<User> findAll() {
         return new ArrayList<>(userMap.values());
